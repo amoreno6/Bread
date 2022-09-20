771bd2d615650327358f0036c75708a273b7bfd3
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-09-20 13:44:27 -0400

    Update BreadTester.java

--- BreadTester.java
+++ BreadTester.java
@@ -1,1 +1,3 @@
-
+https://www.justataste.com/fruit-and-cream-cheese-breakfast-pastries-recipe/
+https://www.allrecipes.com/recipe/17481/simple-white-cake/
+https://www.tastingtable.com/763582/simple-apple-strudel-recipe/
